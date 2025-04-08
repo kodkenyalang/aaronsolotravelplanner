868d20c573ccaaf7a2d9fa76ce983f055d1964ae
kodkenyalang <aaron.ong@mailfence.com>
2025-04-08 18:09:45 +0800

    Create logging.py

---
+++ src/utils/logging.py
@@ -0,0 +1,21 @@
+def setup_logging():
+    """Set up logging for the application"""
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    
+    # Clear any existing handlers to avoid duplicates
+    if logger.hasHandlers():
+        logger.handlers.clear()
+    
+    # Create console handler
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+    
+    # Create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+    
+    # Add handler to logger
+    logger.addHandler(console_handler)
+    
+    return logger
